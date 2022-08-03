9562dfae8ae49fa0f21c91d07816118a9aef823d
PrasathGowtham <108874931+PrasathGowtham@users.noreply.github.com>
2022-08-03 18:32:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-var sandeep=document.createElement("div");
-var span=document.createElement("span");
-var para=document.createElement("p");
-para.innerHTML="hellow world hi new to world";
-span.append(para);
-sandeep.append(span);
-document.body.append(sandeep);
-
-var ele=document.createElement("div");
-var span=document.createElement("span");
-var p=document.createElement("p");
-p.innerHTML="this is p";
-span.append(p);
-ele.append(span);
-document.body.append(ele);
-
-
-var ele=document.createElement("div");
-var span=document.createElement("span");
-span.innerHTML="<p>this is p </p>";
-ele.append(span);
-document.body.append(ele);
-
-
-const message = function() {  
-    console.log("This message is shown after 3 seconds");
+function createlable(tagname,attname,attvalue,content){
+    var lab=document.createElement("tagname");
+    lab.setAttribute(attname,attvalue);
+    lab.innerHTML=content;
+    return lab
 }
-setTimeout(message, 3000);
+
+function createinput(tag,types,cont){
+var input=document.createElement("input");
+input.setAttribute(types,cont);
+return input;
+}
+
+function createbreak(braker){
+    var nextline=document.createElement("br");
+    return nextline;
+}
+
+
+var labelfirst=createlable("label","for","firstname","Firstname");
+var breaker=createbreak("br");
+var inputfirst=createinput("input","type","text");
+var breaker1=createbreak("br");
+var labellast=createlable("label","for","firstname","Lastname");
+var breaker2=createbreak("br");
+var inputlast=createinput("input","type","text");
+var breaker3=createbreak("br");
+var labelemail=createlable("label","for","email","Email");
+var breaker4=createbreak("br");
+var inputemail=createinput("input","type","text");
+var breaker5=createbreak("br");
+var label=createlable("label","for","number","Phonenumber");
+var breaker6=createbreak("br");
+var input=createinput("input","type","tel");
+document.body.append(labelfirst,breaker,inputfirst,breaker1,labellast,breaker2,inputlast,breaker3,labelemail,breaker4,inputemail,breaker5,label,breaker6,input);
